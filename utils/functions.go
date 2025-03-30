8a197e6fcd152a79e581dc0acb5b8f45b8f9ae61
tesayat <temsay.krg@gmail.com>
2025-03-30 21:38:34 +0500

    error handling of sensitive input

--- utils/functions.go
+++ utils/functions.go
@@ -41,7 +41,7 @@ func Input() error {
 			os.Exit(1)
 		}
 
-		if w < 2 || h < 2 {
+		if originalW < 3 || originalH < 3 {
 			return fmt.Errorf("invalid grid size. Minimum size is 3x3")
 		}
 
@@ -257,13 +257,14 @@ func PrintHelp() {
 	fmt.Println(`Usage: go run main.go [options]
 
 Options:
-  --help            : Show this message and exit
-  --verbose         : Display tick number, grid size, delay time, and live cell count
-  --delay-ms=DELAY  : Set the delay time in milliseconds (accepts only integer values). Default is 2500
-  --random=WxH      : Generate a random grid of the specified width (W) and height (H)
-  --footprints      : Add traces of visited cells, displayed as '∘'
-  --colored         : Add color to live cells and traces if footprints are enabled
-  --fullscreen      : Adjust the grid to fit the terminal size with empty cells
-  --file=X          : Load the initial grid from a specified file
+  --help        : Show the help message and exit
+  --verbose     : Display detailed information about the simulation, including grid size, number of ticks, speed, and map name
+  --delay-ms=X  : Set the animation speed in milliseconds. Default is 2500 milliseconds
+  --file=X      : Load the initial grid from a specified file
+  --edges-portal: Enable portal edges where cells that exit the grid appear on the opposite side
+  --random=WxH  : Generate a random grid of the specified width (W) and height (H)
+  --fullscreen  : Adjust the grid to fit the terminal size with empty cells
+  --footprints  : Add traces of visited cells, displayed as '∘'
+  --colored     : Add color to live cells and traces if footprints are enabled
 `)
 }
