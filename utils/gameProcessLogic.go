f0115bf366cd9319131e3090edeb0caaed2f2b96
Keneke-Einar <khabdrazakovkenesary@gmail.com>
2025-03-30 23:45:25 +0500

    func's access reworked

--- utils/gameProcessLogic.go
+++ utils/gameProcessLogic.go
@@ -2,29 +2,26 @@ package utils
 
 import (
 	"fmt"
-	"math/rand"
-	"strconv"
-	"strings"
 	"time"
 )
 
 // Runs the game simulation loop until no live cells remain
 func RunGame() {
 	for {
-		PrintMap()
+		printMap()
 
 		if CountLiveCells() == 0 {
 			break
 		}
 
-		UpdateMap()
+		updateMap()
 
 		time.Sleep(time.Duration(Config.Delay) * time.Millisecond)
 	}
 }
 
 // Clears the console and prints the current game grid
-func PrintMap() {
+func printMap() {
 	ClearConsole()
 
 	if Config.Verbose {
@@ -47,7 +44,7 @@ DelayMs: %vms
 }
 
 // Applies game rules to update the grid state
-func UpdateMap() {
+func updateMap() {
 	newMap := make([][]rune, h)
 	for i := range newMap {
 		newMap[i] = make([]rune, w)
@@ -74,57 +71,4 @@ func UpdateMap() {
 	}
 
 	gameMap = newMap
-}
-
-// Generates a random game map with specified dimensions
-func GenerateRandomMap(dimensions string) error {
-	parts := strings.Split(dimensions, "x")
-	if len(parts) != 2 {
-		return fmt.Errorf("Error: invalid format for --random flag. Use --random=WxH")
-	}
-
-	width, errW := strconv.Atoi(parts[0])
-	height, errH := strconv.Atoi(parts[1])
-
-	if errW != nil || errH != nil || width <= 0 || height <= 0 {
-		return fmt.Errorf("Error: invalid dimensions for --random flag. Width and height must be positive integers.")
-	}
-
-	if width < 3 || height < 3 {
-		return fmt.Errorf("invalid grid size. Minimum size is 3x3")
-	}
-
-	if Config.Fullscreen {
-		termWidth, termHeight = GetTerminalSize()
-
-		effectiveHeight := termHeight
-		if Config.Verbose {
-			effectiveHeight -= 5
-		}
-
-		if height < effectiveHeight {
-			height = effectiveHeight
-		}
-		if width < termWidth {
-			width = termWidth
-		}
-	}
-
-	w, h = width, height
-	gameMap = make([][]rune, h)
-
-	rand.Seed(time.Now().UnixNano())
-	for i := 0; i < h; i++ {
-		row := make([]rune, w)
-		for j := 0; j < w; j++ {
-			if rand.Intn(2) == 0 {
-				row[j] = '#'
-			} else {
-				row[j] = '.'
-			}
-		}
-		gameMap[i] = row
-	}
-
-	return nil
 }
